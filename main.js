220d98dfc75aed7c9b30d1e95edb47a527c6df55
James White <cyberyt@gmail.com>
2015-08-16 20:34:29 +0100

    blergh

--- main.js
+++ main.js
@@ -15,20 +15,38 @@ var gameBoard = [
                 ['','',''],
                 ['','','']
 ];
-function wireBoard() {
-  for(row = 0; row <=2; row++) {
-    for(col = 0; col <=2; col++) {
-      gameBoard[row][col] = document.getElementById('r'+(row+1)+'-c'+(col+1));
-    }
+
+
+var r1c1 = document.getElementById('r1c1');
+var r1c2 = document.getElementById('r1c2');
+var r1c3 = document.getElementById('r1c3');
+var r2c1 = document.getElementById('r2c1');
+var r2c2 = document.getElementById('r2c2');
+var r2c3 = document.getElementById('r2c3');
+var r3c1 = document.getElementById('r3c1');
+var r3c2 = document.getElementById('r3c2');
+var r3c3 = document.getElementById('r3c3');
+
+r1c1.addEventListener('click', turn);
+r1c2.addEventListener('click', turn);
+r1c3.addEventListener('click', turn);
+r2c1.addEventListener('click', turn);
+r2c2.addEventListener('click', turn);
+r2c3.addEventListener('click', turn);
+r3c1.addEventListener('click', turn);
+r3c2.addEventListener('click', turn);
+r3c3.addEventListener('click', turn);
+
+var counter = 0;
+
+function turn() {
+  if (counter%2===0) {
+    var player = 'X';
+    placeMove(player)
   }
 }
-wireBoard();
-//Not working, can't fathom why. 
 
-// var gameBoard = [
-//                 ['','',''],
-//                 ['','',''],
-//                 ['','','']
-// ];
+function placeMove(player, row, col) {
+  gameBoard[row][col] = player;
+}
 
-// gameBoard[0][0] = document.getElementById('r1-c1');
